eef0733620190f586e36ce292289fd6652ab079b
ultrletech <ultrletech@gmail.com>
2026-06-18 15:54:50 +0000

    fix pdfjs build error for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['groq-sdk'],
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
